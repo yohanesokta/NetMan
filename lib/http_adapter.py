939b1bcc22b7b8cb4e14d4d61a0b89faaf3694e4
yohanesokta <yohanesoktayt72@gmail.com>
2025-12-07 22:36:29 +0700

    add adapter class

--- lib/http_adapter.py
+++ lib/http_adapter.py
@@ -1,17 +1,20 @@
 import requests
 
 class HttpAdapter:
+    def __init__(self):
+        self.request = requests
+
     def fetch(self, url, method='GET', headers=None, body=None, body_type='json'):
         try:
             if body_type == 'json':
-                response = requests.request(method, url, headers=headers, json=body)
-            else: # form data
-                response = requests.request(method, url, headers=headers, data=body)
-            
-            response.raise_for_status()  # Raise an exception for bad status codes
+                response = self.request.request(method, url, headers=headers, json=body)
+            else:
+                response = self.request.request(method, url, headers=headers, data=body)
+            response.raise_for_status()
             try:
                 return response.json()
             except requests.exceptions.JSONDecodeError:
                 return response.text
+
         except requests.exceptions.RequestException as e:
             return {"Errors": "Failed Request", "Errors Details": str(e)}
